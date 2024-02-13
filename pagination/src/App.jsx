f36e1f8c72333ae71baffe576857d7a7116e97c7
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-12 22:06:41 -0300

    feat: function fetchProducts

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
+  async function fetchProducts() {
+    const res = await fetch(`https://dummyjson.com/products?limit=100`);
+    const data = await res.json();
+
+    console.log(data && data.products);
+
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
     <>
       <p>Hello Word</p>
