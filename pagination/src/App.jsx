2b28c1b0ee598506b809bd4db910cc2475d3717b
DanielSiilva <daniellsousa0614@gmail.com>
2024-02-12 22:17:44 -0300

    style: home

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     }
   }
 
-  function selectPageHander(selectedPage) {
+  function selectPageHandler(selectedPage) {
     if (
       selectedPage >= 1 &&
       selectedPage <= products.length / 10 &&
@@ -32,7 +32,53 @@ function App() {
   }, []);
   return (
     <>
-      <p>Hello Word</p>
+      <div>
+        {products.length > 0 && (
+          <div className="products">
+            {products.slice(page * 10 - 10, page * 10).map((prod) => {
+              return (
+                <span className="products__single" key={prod.id}>
+                  <img src={prod.thumbnail} alt={prod.title} />{" "}
+                  {/* alt is imp */}
+                  <span>{prod.title}</span>
+                </span>
+              );
+            })}
+          </div>
+        )}
+
+        {products.length > 0 && (
+          <div className="pagination">
+            <span
+              onClick={() => selectPageHandler(page - 1)}
+              className={page > 1 ? "" : "pagination__disable"}
+            >
+              ◀
+            </span>
+
+            {[...Array(products.length / 10)].map((_, i) => {
+              return (
+                <span
+                  key={i}
+                  className={page === i + 1 ? "pagination__selected" : ""}
+                  onClick={() => selectPageHandler(i + 1)}
+                >
+                  {i + 1}
+                </span>
+              );
+            })}
+
+            <span
+              onClick={() => selectPageHandler(page + 1)}
+              className={
+                page < products.length / 10 ? "" : "pagination__disable"
+              }
+            >
+              ▶
+            </span>
+          </div>
+        )}
+      </div>
     </>
   );
 }
